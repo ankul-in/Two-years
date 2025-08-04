6ec7f4fe276b9102203e5c54befe9bb4e4d5e56a
Your Name <your_email@example.com>
2025-08-04 05:47:18 +0530

    10

--- armstrong.py
+++ armstrong.py
@@ -1,1 +1,14 @@
 #program to find armstrong number 
+
+num=int(input("enter the number to check if its armstrong-->"))
+temp=num
+len=len(str(num))
+arms=0
+for i in range(len):
+    remainder=num%10
+    num=num//10
+    arms=arms+(remainder**len)
+if arms==temp:
+    print("number is armstrong")
+else:
+    print("number is Not armstrong")
