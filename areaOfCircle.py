1af66568b55508b247f5d0c9df22235d285fa2d4
Your Name <your_email@example.com>
2025-08-04 06:57:59 +0530

    16

---
+++ areaOfCircle.py
@@ -0,0 +1,4 @@
+#15 area oof circle
+radius=float(input("enter radius of circle-->"))
+area=3.14*radius*radius
+print(area,"unit sq.")
---
+++ stringReverse.py
@@ -0,0 +1,11 @@
+#16 reverse a string
+# import string 
+string=input("enter your string to reverse it-->")
+# reverse=[]
+# for i in string:
+#     reverse.insert(0,i)
+# print(''.join(reverse))
+
+
+reversestr=string[::-1]
+print(reversestr)
