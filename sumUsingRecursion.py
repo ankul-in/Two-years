31a4083ca3a396cc8d8c7929e04c7f727c72fbf3
Ankul <ankulin@outlook.com>
2025-08-10 20:59:17 +0530

    29

--- sumUsingRecursion.py
+++ sumUsingRecursion.py
@@ -1,7 +1,10 @@
 #sum using recursion
-def recursiveSum(arr):
-    if not arr: 
-        return 0
-    return arr[0] + recursiveSum(arr[1:])
+# def recursiveSum(arr):
+#     if not arr: 
+#         return 0
+#     return arr[0] + recursiveSum(arr[1:])
 
-print(recursiveSum([1, 2, 3, 4, 5]))
+# print(recursiveSum([1, 2, 3, 4, 5]))
+def sum(xs, i=0, s=0):
+  return s if i >= len(xs) else sum(xs, i+1, s+xs[i])
+print(sum([1,2,3,4]))
