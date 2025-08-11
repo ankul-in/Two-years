5e3123915d89c9349a8b585c501859c7ac1bdbe7
Ankul <ankulin@outlook.com>
2025-08-11 23:58:33 +0530

    48

---
+++ mangoSellerProblemBFS.py
@@ -0,0 +1,37 @@
+#a graph problem
+from collections import deque
+graph = {}
+graph['you'] = ['alice', 'bob', 'claire']
+graph['bob'] = ['anuj', 'peggy']
+graph['alice'] = ['peggy']
+graph['claire'] = ['thom', 'jonny']
+graph['anuj'] = []
+graph['peggy'] = []
+graph['thom'] = []
+graph['jonny'] = []
+#print("the graph include-->",graph)
+#mango seller is one whos name ends with y
+def mangoSeller(name):
+    return name[-1]=="y"
+
+def searchPeople(people):
+    searchQueue=deque()
+    searchQueue += graph['you']
+    searched=[]
+    while searchQueue:
+        
+        
+        person=searchQueue.popleft()
+        if not person in searched:
+            if mangoSeller(person):
+                print("mango Seller is-->",person)
+                return True
+            else:
+                searchQueue=searchQueue+graph[person]
+                searched.append(person)
+        return False
+    
+searchPeople("you")
+
+
+    
