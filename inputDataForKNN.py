cba87a0a1ff60744a48982657422e12360007c37
Ankul <ankulin@outlook.com>
2025-08-17 21:48:49 +0530

    80

--- inputDataForKNN.py
+++ inputDataForKNN.py
@@ -1,5 +1,21 @@
-#input data points
+# #input data points for knn algorithm
+# import numpy as np
+# n=int(input("enter number of rows-->"))
+# arr=np.array([list(map(int,input("enter data point-->").split())) for i in range(n)])
+# print(arr)
+
 import numpy as np
-n=int(input("enter number of rows-->"))
-arr=np.array([list(map(int,input("enter data point-->").split())) for i in range(n)])
-print(arr)
+
+n = int(input("Enter number of rows --> "))
+arr = []
+
+for i in range(n):
+    row = list(map(int, input(f"Enter data point {i+1} --> ").split()))
+    if i == 0:
+        dim = len(row)
+    elif len(row) != dim:
+        raise ValueError(f"Row {i+1} must have {dim} elements.")
+    arr.append(row)
+
+arr = np.array(arr)
+print("Data points:\n", arr)
