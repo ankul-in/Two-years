3d34f5cb626ad18f8c365d93c58063a1bc4a54c3
Ankul <ankulin@outlook.com>
2025-08-18 23:26:12 +0530

    90

---
+++ KATA24.py
@@ -0,0 +1,7 @@
+#kata https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/python
+def summation(num):
+    sum=0
+    for i in range(num+1):
+        sum+=i
+    return sum
+#i'm low on programs
