c54a7d608833059e93e889fe45d9c6c8b8faf8cb
Ankul <ankulin@outlook.com>
2025-08-20 20:51:22 +0530

    97

---
+++ LinkedList.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.length=0
+    def append(self,value):
+        newNode=Node(value)
+        if self.head is None:
+            self.head=newNode
+            self.tail=newNode
+        else:
+            self.tail.next=newNode
+            self.tail=newNode
+
+    def __str__(self):
+        tempNode=self.head
+        result=""
+        while tempNode is not None:
+            result+=str(tempNode.value)
+            if tempNode.next is not None:
+                result+="-->"
+            tempNode=tempNode.next
+        return result
+    
+new_linked_list = LinkedList()
+new_linked_list.append(10)
+new_linked_list.append(20)
+new_linked_list.append(30)
+print(new_linked_list)
