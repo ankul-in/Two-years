94e7c689cf2a81912a9d640092cdf473606ee371
Ankul <ankulin@outlook.com>
2025-08-20 23:20:30 +0530

    101

---
+++ queueImplementation.py
@@ -0,0 +1,13 @@
+#implementation of queue
+queue=[]
+queue.append("a")
+queue.append("b")
+queue.append("c")
+print("initial queue")
+print(queue)
+print("\nElements dequeud from queue")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+print("\nQueue after removing elements")
+print(queue)
