9867cd21aba84cb0739cbdece9a5c4bd16ac3f6c
Ankul <ankulin@outlook.com>
2025-08-20 23:16:09 +0530

    100

---
+++ Stack.py
@@ -0,0 +1,13 @@
+#implementation of stack
+stack=[]
+stack.append("a")
+stack.append("b")
+stack.append("c")
+print("initial stack")
+print(stack)
+print("\nElelments popped from stack:")
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print("\nStacck after elements are popped")
+print(stack)
