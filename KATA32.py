c463e84d391ec6964854cdcca3bbe2d055e70d7d
Ankul <ankulin@outlook.com>
2025-08-21 23:12:37 +0530

    104

---
+++ KATA32.py
@@ -0,0 +1,16 @@
+def sort_the_inner_content(words):
+    word=words.split()
+    newlist=[]
+    for i in word:
+        
+        x=(sorted(i,reverse=True))
+        newlist.append("".join(x))
+    return print(" ".join(newlist))
+
+sort_the_inner_content("sort the inner content in descending order")
+# def sort_str(words):
+#     new_words = words.split()
+#     return " ".join(sorted(new_words, reverse=True))
+
+# # prints 'Charlie Bob Alice'
+# print(sort_str("Charlie Alice Bob"))
