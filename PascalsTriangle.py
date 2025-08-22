e9888e736349b1cb8398f76a19c275b0d6521429
Ankul <ankulin@outlook.com>
2025-08-22 22:14:37 +0530

    115

---
+++ PascalsTriangle.py
@@ -0,0 +1,9 @@
+#pascals triangle print
+from math import factorial
+n=int(input("enter number of rows-->"))
+for i in range(n):
+    for j in range(n-i+1):
+        print(end=" ")
+    for j in range(i+1):
+        print(factorial(i)//factorial(j)*factorial(i-j),end=" ")
+    print()
