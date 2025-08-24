6f050179cab43a5308696f04f289594c3f8d1ebc
Ankul <ankulin@outlook.com>
2025-08-24 19:40:49 +0530

    119

---
+++ G&G4.py
@@ -0,0 +1,7 @@
+#https://www.geeksforgeeks.org/problems/first-digit-of-a-number/1&selectedLang=python3
+
+n=int(input("enter your number-->"))
+print("first digit is -->",end="")
+for i in str(n):
+    print(i)
+    break
