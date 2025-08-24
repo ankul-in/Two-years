30bb3e686d4d66c377433e652006f7ac4e85ca6b
Ankul <ankulin@outlook.com>
2025-08-24 19:41:11 +0530

    119

---
+++ G&G5.py
@@ -0,0 +1,5 @@
+#https://www.geeksforgeeks.org/problems/print-gfg-n-times/1&selectedLang=python3?page=1
+
+n=int(input("enter number of times you wanna print gfg-->"))
+for i in range(n):
+    print("GFG",end=" ")
