8ae4656d2b10200b6f78a729026b824bee828787
Ankul <ankulin@outlook.com>
2025-08-24 19:39:09 +0530

    118

---
+++ G&G3.py
@@ -0,0 +1,8 @@
+#https://www.geeksforgeeks.org/problems/function-with-arguments-1605762259/1&selectedLang=python3
+
+def arguentFunction(a,b):
+    return print(a+b)
+
+a=int(input("enter a-->"))
+b=int(input("enter b-->"))
+arguentFunction(a,b)
