3902052a5f2a29ff0bf4791946e4e6066f31559b
Ankul <ankulin@outlook.com>
2025-08-24 19:38:39 +0530

    116

---
+++ G&G1.py
@@ -0,0 +1,6 @@
+#https://www.geeksforgeeks.org/problems/function-with-return-value/1&selectedLang=python3
+
+def returnValueFunction(n):
+    return print(2*n)
+n=int(input("enteryournumber-->"))
+returnValueFunction(n)
