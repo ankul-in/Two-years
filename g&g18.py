825def92fb9470923844625cb1bd8615d0d60356
Ankul <ankulin@outlook.com>
2025-08-26 23:45:30 +0530

    135

--- g&g18.py
+++ g&g18.py
@@ -23,4 +23,21 @@ for i in a:
         intersection.append(i)
 
 #print(intersection)
-
+dict1={1: [1], 9: [4, 5], 14: [6, 8]}
+dict2={5: [2, 3], 4: [4], 15: [6, 9]}
+result=[]
+# for i,j in dict1.items():
+#     for k,l in dict2.items():
+#         if i>k:
+#             result.append(dict1[i])
+#             break
+#         else:
+#             result.append(dict2[k])
+#             break
+zipAns=zip(dict1,dict2)
+for i,j in list(zipAns):
+    if i>j:
+        result.append(dict1[i])
+    else:
+        result.append(dict2[j])
+print(result)
