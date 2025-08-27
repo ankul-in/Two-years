51e6eababbe876ee2828b8aa3bcd1a1db3ff241a
Ankul <ankulin@outlook.com>
2025-08-27 20:14:41 +0530

    136

--- g&g19.py
+++ g&g19.py
@@ -1,11 +1,14 @@
+#https://www.geeksforgeeks.org/problems/permutations-of-a-given-string2041/1&selectedLang=python3
+#forgot
+
 s=input("enter your string-->")
 list1=list(s)
 print(list1)
 
 from itertools import permutations
 perms=permutations(list1,4)
-print(list(perms))
-print(len(list(perms)))
+# print(list(perms))
+# print(len(list(perms)))
 result=[]
 for i in list(perms):
     item="".join(i)
