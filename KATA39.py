39ffe2d15c20caab326ef7ecaf9a8ec85e1d9b02
Ankul <ankulin@outlook.com>
2025-08-27 21:14:16 +0530

    138

---
+++ KATA39.py
@@ -0,0 +1,13 @@
+#Kata 
+# 
+# https://www.codewars.com/kata/5535572c1de94ba2db0000f6/train/python
+# 
+class Dog:
+    def __init__(self, name ,breed ,sex,age):
+        self.name = name
+        
+
+def bark(self):
+    return "Woof!" 
+
+Dog.bark = bark
