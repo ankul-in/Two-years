76d92368ea0ffebdaa7f0a1a2cc00aa879ee9aaa
Ankul <ankulin@outlook.com>
2025-08-29 23:08:54 +0530

    146

---
+++ KATA41.py
@@ -0,0 +1,17 @@
+text = """\
+Lorem  ipsum  dolor  sit amet,consectetur  adipiscing  elit.Vestibulum    sagittis   dolormauris,  at  elementum  ligula
+tempor  eget.  In quis rhoncusnunc,  at  aliquet orci. Fusceat   dolor   sit   amet  felissuscipit   tristique.   Nam  a
+imperdiet   tellus.  Nulla  euvestibulum    urna.    Vivamustincidunt  suscipit  enim, necultrices   nisi  volutpat  ac.
+Maecenas   sit   amet  laciniaarcu,  non dictum justo. Donecsed  quam  vel  risus faucibuseuismod.  Suspendisse  rhoncus
+rhoncus  felis  at  fermentum.Donec lorem magna, ultricies anunc    sit    amet,   blanditfringilla  nunc. In vestibulumvelit    ac    felis   rhoncus
+pellentesque. Mauris at tellusenim.  Aliquam eleifend tempusdapibus. Pellentesque commodo,
+nisi    sit   amet   hendreritfringilla,   ante  odio  portalacus,   ut   elementum  justonulla et dolor."""
+
+
+# def justify(text, width):
+#     strlist=list(text.split())
+#     for i in strlist:
+#         while length < width:
+            
+#     print(text)
+# justify(text,30)
