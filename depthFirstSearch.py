b2646dea3e3b2d058efde9d2ced3a1656cde19b6
Ankul <ankulin@outlook.com>
2025-08-31 20:43:51 +0530

    148

--- depthFirstSearch.py
+++ depthFirstSearch.py
@@ -102,34 +102,89 @@
 ##########################################################################################
 
 
+# from collections import defaultdict
+# class Graph:
+#     def __init__(self):
+#         self.graph=defaultdict(list)
+#     def add_edge(self,u,v):
+#         self.graph[u].append(v)
+#     def dfs_iterative(self,start):
+#         visited=set()
+#         stack=[start]
+#         while stack:
+#             node=stack.pop()
+#             if node not in visited:
+#                 print(node,end=" ")
+#                 visited.add(node)
+#                 for neighbor in reversed(self.graph[node]):
+#                     if neighbor not in visited:
+#                         stack.append(neighbor)
+
+# g=Graph()
+# g.add_edge(0,1)
+# g.add_edge(0,2)
+# g.add_edge(1,3)
+# g.add_edge(1,4)
+# g.add_edge(2,5)
+# print("itrative DFS starting from vertex 0:")
+# g.dfs_iterative(0)
+
+
+
+# # i didint learn anything yet 
+
+
+# from collections import defaultdict
+# class Graph:
+#     def __init__(self):
+#         self.graph=defaultdict(list)
+#     def add_edge(self,u,v):
+#         self.graph[u].append(v)
+#     def dfs_iterative(self,start):
+#         visited=set()
+#         stack=[start]
+#         while stack:
+#             node=stack.pop()
+#             if node not in visited:
+#                 print(node,end=" ")
+#                 visited.add(node)
+#                 for neighbor in reversed(self.graph[node]):
+#                     if neighbor not in visited:
+#                         stack.append(neighbor)
+
+# g=Graph()
+# g.add_edge(0,1)
+# g.add_edge(0,2)
+# g.add_edge(1,3)
+# g.add_edge(1,4)
+# g.add_edge(2,5)
+# print("itrative DFS starting from vertex 0:")
+# g.dfs_iterative(0)
+
+
 from collections import defaultdict
-class Graph:
-    def __init__(self):
-        self.graph=defaultdict(list)
-    def add_edge(self,u,v):
-        self.graph[u].append(v)
-    def dfs_iterative(self,start):
-        visited=set()
-        stack=[start]
-        while stack:
-            node=stack.pop()
-            if node not in visited:
-                print(node,end=" ")
-                visited.add(node)
-                for neighbor in reversed(self.graph[node]):
-                    if neighbor not in visited:
-                        stack.append(neighbor)
+def add_edge(adj,s,t):
+    adj[s].append(t)
+    adj[t].append(s)
 
-g=Graph()
-g.add_edge(0,1)
-g.add_edge(0,2)
-g.add_edge(1,3)
-g.add_edge(1,4)
-g.add_edge(2,5)
-print("itrative DFS starting from vertex 0:")
-g.dfs_iterative(0)
+def dfs_rec(adj,visited,s,res):
+    visited[s]=True
+    res.append(s)
+    for i in adj[s]:
+        if not visited[i]:
+            dfs_rec(adj,visited,i,res)
 
+def dfs(adj):
+    visited=[False]*len(adj)
+    res=[]
+    for i in range(len(adj)):
+        if not visited[i]:
+            dfs_rec(adj,visited,i,res)
 
-# i didint learn anything yet 
-
-
+v=6
+adj=defaultdict(list)
+edges = [[1, 2], [2, 0], [0, 3], [4, 5]]
+for e in edges:
+    add_edge(adj, e[0], e[1])
+res = dfs(adj)
+print("".join(map(str,res)))
