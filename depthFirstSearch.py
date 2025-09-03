babbb12f5445e7e057ae7c90263995a945774ad0
Ankul <ankulin@outlook.com>
2025-09-03 19:23:51 +0530

    148

--- depthFirstSearch.py
+++ depthFirstSearch.py
@@ -1,4 +1,4 @@
-# #dfs
+# #dfs #148
 
 # # # Create an adjacency list for the graph
 # from collections import defaultdict
@@ -220,29 +220,54 @@
 # print(" ".join(map(str,res)))
 
 
+# from collections import defaultdict
+# def add_edge(adj,s,t):
+#     adj[s].append(t)
+#     adj[t].append(s)
+# def dfs_rec(adj,visited,s,res):
+#     visited[s]=True
+#     res.append(s)
+#     for i in adj[s]:
+#         if not visited[i]:
+#             dfs_rec(adj,visited,i,res)
+# def dfs(adj):
+#     visited=[False]*len(adj)
+#     res=[]
+#     for i in range(len(adj)):
+#         if not visited[i]:
+#             dfs_rec(adj,visited,i,res)
+#     return res
+
+# v=6
+# adj=defaultdict(list)
+# edges=[[1,2],[2,0],[0,3],[4,5]]
+# for e in edges:
+#     add_edge(adj,e[0],e[1])
+
+# res=dfs(adj)
+# print(" ".join(map(str,res)))
+
 from collections import defaultdict
-def add_edge(adj,s,t):
+def addEdge(adj,s,t):
     adj[s].append(t)
     adj[t].append(s)
-def dfs_rec(adj,visited,s,res):
+def dfsRec(adj,visited,s,res):
     visited[s]=True
     res.append(s)
     for i in adj[s]:
         if not visited[i]:
-            dfs_rec(adj,visited,i,res)
+            dfsRec(adj,visited,i,res)
 def dfs(adj):
     visited=[False]*len(adj)
     res=[]
     for i in range(len(adj)):
         if not visited[i]:
-            dfs_rec(adj,visited,i,res)
+            dfsRec(adj,visited,i,res)
     return res
-
-v=6
 adj=defaultdict(list)
 edges=[[1,2],[2,0],[0,3],[4,5]]
 for e in edges:
-    add_edge(adj,e[0],e[1])
-
+    addEdge(adj,e[0],e[1])
 res=dfs(adj)
 print(" ".join(map(str,res)))
+#someday ima understand this 
