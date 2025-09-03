973cb69d5fe68677db74c86c96442f824d10651a
Ankul <ankulin@outlook.com>
2025-09-03 19:06:05 +0530

    163

--- slidingWindowGrind.py
+++ slidingWindowGrind.py
@@ -1,18 +1,70 @@
-#sliding window grind
+#sliding window grind  
+#163
+
+# def maxSum(arr, k):
+    
+#     # length of the array
+#     n = len(arr)
+
+#     # n must be greater than k
+#     if n <= k:
+#         print("Invalid")
+#         return -1
+
+#     # Compute sum of first window of size k
+#     window_sum = sum(arr[:k])
+
+#     # first sum available
+#     max_sum = window_sum
+
+#     # Compute the sums of remaining windows by
+#     # removing first element of previous
+#     # window and adding last element of
+#     # the current window.
+#     for i in range(n - k):
+#         window_sum = window_sum - arr[i] + arr[i + k]
+#         max_sum = max(window_sum, max_sum)
+
+#     return max_sum
+
+
+# if __name__ == "__main__":
+#     arr = [5, 2, -1, 0, 3]
+#     k = 3
+#     print(maxSum(arr, k))
+
+
+
+# def maxSum(arr,k):
+#     n=len(arr)
+#     if n <=k:
+#         print("Invalid")
+#         return -1
+#     windowSum=sum(arr[:k])
+#     max_sum=windowSum
+#     for i in range(n-k):
+#         windowSum=windowSum-arr[i]+arr[i+k]
+#         max_sum=max(windowSum,max_sum)
+#     return max_sum
+
+# if __name__=="__main__":
+#     arr=[5,2,-1,0,3]
+#     k=3
+#     print(maxSum(arr,k))
+
+
 def maxSum(arr,k):
     n=len(arr)
-    if n <=k:
-        print("Invalid")
+    if n<=k:
+        print("invalid")
         return -1
     windowSum=sum(arr[:k])
-    max_sum=windowSum
+    maxSum=windowSum
     for i in range(n-k):
         windowSum=windowSum-arr[i]+arr[i+k]
-        max_sum=max(windowSum,max_sum)
-    return max_sum
-
+        maxSum=max(windowSum,maxSum)
+    return maxSum
 if __name__=="__main__":
-    arr=[5,2,-1,0,3]
-    k=3
+    arr=[1,3,4,5,6,7,8,2]
+    k=4
     print(maxSum(arr,k))
-    
