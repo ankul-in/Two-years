a56b135d922796b15ea670a7339c23f682b86143
Ankul <ankulin@outlook.com>
2025-09-08 22:13:30 +0530

    186

---
+++ KATA78.py
@@ -0,0 +1,12 @@
+#kata
+#https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+def no_space(x):
+    y=x.split(" ")
+    return "".join(y)
+#easy peasy
+#i should start attempting higher level quesstions uk feel stuck
+
+#well this was top
+def no_space(x):
+    return x.replace(' ' ,'')
+#☝️good ngl
