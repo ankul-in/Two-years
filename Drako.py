deea53ea77b6aaf2120285e111ad296a5ed74ac1
Ankul <ankulin@outlook.com>
2025-09-08 22:01:43 +0530

    184

---
+++ Drako.py
@@ -0,0 +1,21 @@
+#just some program i liked
+
+
+print("//////////////////////////////")
+print("*******************************")
+print("          Super Draco          ")
+print("//////////////////////////////")
+print("*******************************")
+
+drako = input("How Many times u wanna shoot the drako: ")
+
+def drake():
+    try:
+
+        while True:
+            print(f"Drako Shooting {drako} Amount of times")
+
+    except KeyboardInterrupt:
+        print(f"\nStopped shooting drako")
+    
+drake()
