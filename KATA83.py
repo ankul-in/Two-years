1cfa7c5f538f10bbbafe3988d2d46ab77ddfe5bf
Ankul <ankulin@outlook.com>
2025-09-11 21:06:43 +0530

    193

--- KATA83.py
+++ KATA83.py
@@ -2,6 +2,27 @@
 #kata
 #https://www.codewars.com/kata/59aa6567485a4d03ff0000ca/train/python
 import gmpy2
+
+def list_prime(a, b):
+    return [num for num in range(a, b) if gmpy2.is_prime(num)]
+
+def happy_number(n):
+    seen = set()
+    while True:
+        n = sum(int(digit)**2 for digit in str(n))
+        if n == 1:
+            return True
+        if n in seen:
+            return False
+        seen.add(n)
+
+def solve(a, b):
+    primes = list_prime(a, b)
+    return sum(1 for p in primes if happy_number(p))
+
+#why do i write absolutely monstrosity of a code
+
+import gmpy2
 def list_prime(a,b):
     primelist=[]
     for num in range(a,b):
