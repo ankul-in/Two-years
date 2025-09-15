c89330e5d08bc42763e59605615d6c01563fc7e0
Ankul <ankulin@outlook.com>
2025-09-15 22:32:23 +0530

    211

---
+++ decorator.py
@@ -0,0 +1,13 @@
+
+#decorators
+
+def changecase(func):
+  def myinner(*args, **kwargs):
+    return func(*args, **kwargs).upper()
+  return myinner
+
+@changecase
+def myfunction(nam):
+  return "Hello " + nam
+
+print(myfunction("John"))
