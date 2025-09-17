032e75f357ddc0a0d55ed6f7223ba87068ba5365
Ankul <ankulin@outlook.com>
2025-09-17 22:27:52 +0530

    148

--- depthFirstSearch.py
+++ depthFirstSearch.py
@@ -530,6 +530,31 @@
 # print(" ".join(map(str,res)))
 
 
+# from collections import defaultdict
+# def addEdge(adj,s,t):
+#     adj[s].append(t)
+#     adj[t].append(s)
+# def dfsRec(adj,visited,s,res):
+#     visited[s]=True
+#     res.append(s)
+#     for i in adj[s]:
+#         if not visited[s]:
+#             if not visited[i]:
+#                 dfsRec(adj,visited,i,res)
+# def dfs(adj):
+#     visited=[False]*len(adj)
+#     res=[]
+#     for i in range(len(adj)):
+#         dfsRec(adj,visited,i,res)
+#     return res
+# adj=defaultdict(list)
+# edges=[[1,2],[2,0],[0,3],[4,5]]
+# for e in edges:
+#     addEdge(adj,e[0],e[1])
+# res=dfs(adj)
+# print(" ".join(map(str,res)))
+
+
 from collections import defaultdict
 def addEdge(adj,s,t):
     adj[s].append(t)
