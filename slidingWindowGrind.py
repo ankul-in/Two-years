73ebe615b18659213684a7d78da42f1e1d08c79f
Ankul <ankulin@outlook.com>
2025-09-27 23:33:20 +0530

    163

--- slidingWindowGrind.py
+++ slidingWindowGrind.py
@@ -303,6 +303,19 @@
 #     return maxSum
 # print(maxSum([6,-1,-5,0,-6,4],2))
 
+# def maxSum(arr,k):
+#     n=len(arr)
+#     if n<k:
+#         print("invalid")
+#         return -1
+#     windowSum=sum(arr[:k])
+#     maxSum=windowSum
+#     for i in range(n-k):
+#         windowSum=windowSum-arr[i]+arr[i+k]
+#         maxSum=max(windowSum,maxSum)
+#     return maxSum
+# print(maxSum([1,2,3,4,5,6],3))
+
 def maxSum(arr,k):
     n=len(arr)
     if n<k:
@@ -314,4 +327,4 @@ def maxSum(arr,k):
         windowSum=windowSum-arr[i]+arr[i+k]
         maxSum=max(windowSum,maxSum)
     return maxSum
-print(maxSum([1,2,3,4,5,6],3))
+print(maxSum([5,6,3,-1,-4,5],4))
