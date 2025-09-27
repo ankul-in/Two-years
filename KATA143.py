655a199de338615b9c4be6007cbc42abb9c143f9
Ankul <ankulin@outlook.com>
2025-09-27 23:29:31 +0530

    271

---
+++ KATA143.py
@@ -0,0 +1,19 @@
+#kata
+#https://www.codewars.com/kata/584d2c19766c2b2f6a00004f/train/python
+# def func_or(a,b):
+#     if a == True:
+#         if b == False:
+#             return True
+#     if b == True:
+#         if a == False:
+#             return True
+#     return False
+
+# def func_xor(a,b):
+#     if a == True:
+#         return True
+#     if b == True:
+#         return True
+#     return False
+
+print(func_or(True, True))
