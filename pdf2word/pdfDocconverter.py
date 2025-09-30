4ab3183f85b878c104580eee6f29bb8456cd9d37
Ankul <ankulin@outlook.com>
2025-09-30 23:59:00 +0530

    285

--- pdf2word/pdfDocconverter.py
+++ pdf2word/pdfDocconverter.py
@@ -1,1 +1,10 @@
-#import
+#https://youtu.be/fJ-dzdlejyc
+from pdf2docx import Converter
+
+old_pdf = r"D:\PY\pdf2word\Cisco.pdf"
+new_doc = r"new.docx"
+
+
+obj=Converter(old_pdf)
+obj.convert(new_doc)
+obj.close()
