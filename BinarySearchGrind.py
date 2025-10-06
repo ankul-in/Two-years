f7507a3e219cb8925c49026a0948bfc3a6fe1380
Ankul <ankulin@outlook.com>
2025-10-06 23:38:29 +0530

    145

--- BinarySearchGrind.py
+++ BinarySearchGrind.py
@@ -298,6 +298,21 @@
 # print(binarySearch([1,2,3,4,5,6],2))
 
 
+# def binarySearch(arr,k):
+#     start,end=0,len(arr)-1
+#     while start<=end:
+#         mid=start+(end-start)//2
+#         if arr[mid]==k:
+#             return mid
+#         elif arr[mid]<k:
+#             start=mid+1
+#         else:
+#             end=mid-1
+#     return -1
+# print(binarySearch([1,2,3,4,5,6],5))
+
+
+
 def binarySearch(arr,k):
     start,end=0,len(arr)-1
     while start<=end:
@@ -309,4 +324,4 @@ def binarySearch(arr,k):
         else:
             end=mid-1
     return -1
-print(binarySearch([1,2,3,4,5,6],5))
+print(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12],12))
