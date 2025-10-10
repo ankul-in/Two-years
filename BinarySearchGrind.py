a78b1f3fba643ad7ebcf29fe364f4a2fd7e34d1b
Ankul <ankulin@outlook.com>
2025-10-10 21:29:42 +0530

    145

--- BinarySearchGrind.py
+++ BinarySearchGrind.py
@@ -361,15 +361,30 @@
 
 
 
-def binarySearch(arr,k):
-    start,end=0,len(arr)-1
-    while start<=end:
-        mid=start+(end-start)//2
-        if arr[mid]==k:
-            return mid
-        elif arr[mid]<k:
-            start=mid+1
-        else:
-            end=mid-1
-    return -1
-print(binarySearch([-5,-4,-3,-2,-1],-3))
+# def binarySearch(arr,k):
+#     start,end=0,len(arr)-1
+#     while start<=end:
+#         mid=start+(end-start)//2
+#         if arr[mid]==k:
+#             return mid
+#         elif arr[mid]<k:
+#             start=mid+1
+#         else:
+#             end=mid-1
+#     return -1
+# print(binarySearch([-5,-4,-3,-2,-1],-3))
+
+
+def maxSum(arr,k):
+    n=len(arr)
+    if n<k:
+        print("invalid")
+        return -1
+    windowSum=sum(arr[:k])
+    maxSum=windowSum
+    for i in range(n-k):
+        windowSum=windowSum-arr[i]+arr[i+k]
+        maxSum=max(windowSum,maxSum)
+
+    return maxSum
+print(maxSum([-1,-2,-3,-4,-5,-6,-7,-8],5))
