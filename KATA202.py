ecad356e601c69e43c90e28f7ff4d2198aeea1e6
Ankul <ankulin@outlook.com>
2025-10-19 21:56:56 +0530

    345

---
+++ KATA202.py
@@ -0,0 +1,6 @@
+#kata
+#https://www.codewars.com/kata/5a026a9cffe75fbace00007f/train/python
+
+import math
+def circle_diameter(sides, side_length): 
+    return (side_length)/(math.tan(math.pi/sides))
