19e01c170d9ed90cb8fa9f25e57e4aa6cff95bb0
Ankul <ankulin@outlook.com>
2025-11-09 23:23:40 +0530

    428

---
+++ KATA284.py
@@ -0,0 +1,11 @@
+#kata
+#https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/solutions/python
+
+def plural(n):
+    return n==0 or n>1 
+
+def plural(n):
+    if n == 0 or n > 1:
+        return True
+    else:
+        return False
