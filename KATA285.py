1180398c6be51c681e92b2db3ad9b55335a7ffc0
Ankul <ankulin@outlook.com>
2025-11-09 23:23:54 +0530

    429

---
+++ KATA285.py
@@ -0,0 +1,11 @@
+#kata
+#https://www.codewars.com/kata/54fe05c4762e2e3047000add/train/python
+
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+    def is_worth_it(self):
+        return self.draft-(1.5*self.crew)>20
+    
+
