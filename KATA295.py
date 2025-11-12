385bc3876b0038db929a8c54cd41ef6f73f9f33a
Ankul <ankulin@outlook.com>
2025-11-12 21:26:08 +0530

    438

---
+++ KATA295.py
@@ -0,0 +1,7 @@
+#kata
+#https://www.codewars.com/kata/529eef7a9194e0cbc1000255/train/python
+
+def is_anagram(test, original):
+    return sorted(test.lower()) == sorted(original.lower())
+
+#dont you feel ashamed ankul?? fckin moron
