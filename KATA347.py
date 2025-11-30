9628a7e92df39b81022f7c35542cf4c412512280
Ankul <ankulin@outlook.com>
2025-11-30 20:24:02 +0530

    492

---
+++ KATA347.py
@@ -0,0 +1,12 @@
+#kata
+#https://www.codewars.com/kata/559e3224324a2b6e66000046/train/python
+
+def sumin(n):
+    return n*(n+1)*(2*n+1)//6
+
+def sumax(n):
+    return n*(n+1)*(4*n-1)//6
+
+def sumsum(n):
+    return n*n*(n+1)
+
