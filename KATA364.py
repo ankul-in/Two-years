f2774d804f666a373e7b82fb59218cd2e877e220
Ankul <ankulin@outlook.com>
2025-12-06 23:35:44 +0530

    509

---
+++ KATA364.py
@@ -0,0 +1,7 @@
+#kata
+#https://www.codewars.com/kata/564f458b4d75e24fc9000041/train/python
+
+def remainder(dividend,divisor):
+    quotient = dividend // divisor
+    return dividend - (quotient * divisor)
+
