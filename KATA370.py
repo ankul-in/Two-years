3a00d2a83f657550687781a76d8e8d98bc23cb1d
Ankul <ankulin@outlook.com>
2025-12-08 23:45:49 +0530

    515

---
+++ KATA370.py
@@ -0,0 +1,14 @@
+#kata
+#https://www.codewars.com/kata/5d62961d18198b000e2f22b3/train/python
+
+def generate_number(squad, n):
+    if n not in squad:
+        return n
+    for i in range(1, 10):       
+        for j in range(1, 10):
+            if i + j == n:
+                candidate = int(f"{i}{j}")
+                if candidate not in squad:
+                    return candidate
+    return None
+
